25aae123001ec62ddb8d6890df2d7751f2b95bdb
Coco martin <150522458+joshuaAposto@users.noreply.github.com>
2024-12-06 13:56:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -27,10 +27,14 @@ routes.forEach(route => {
 app.use((req, res, next) => {
   const userAgent = req.headers['user-agent'];
   if (!userAgent || userAgent.toLowerCase().includes('sitemode') || userAgent.toLowerCase().includes('bot')) {
-    res.status(403).send('<h1>This is a Anti Bogart</h1>');
+    res.status(403).send('<h1>This is a bot</h1>');
   } else {
     next();
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(PORT, () => {
